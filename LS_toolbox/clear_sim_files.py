a69c5665e49737e8c290f574daef265eddf33187
gardegaront <m.gardegaront@gmail.com>
2024-10-07 16:59:01 +0200

    Added a verbose option

--- LS_toolbox/clear_sim_files.py
+++ LS_toolbox/clear_sim_files.py
@@ -7,7 +7,7 @@ sim_files_names_all = ("d3dump", "d3hsp", "d3plot+", "messag", "bndout", "glstat
                        "dyna.inc", "group_file", "kill_by_pid", "load_profile.+", "lspost.+", "mes+", "nodelist.+",
                        "process.log")
 
-def clear_sim_files(sim_dir_path: str, sim_files_names: list = sim_files_names_all):
+def clear_sim_files(sim_dir_path: str, sim_files_names: list = sim_files_names_all, verbose: bool = False):
     """
     Clear simulation files in the given directory.
     :param sim_dir_path: Path to the simulation directory.
@@ -18,7 +18,8 @@ def clear_sim_files(sim_dir_path: str, sim_files_names: list = sim_files_names_a
                 if os.path.isdir(os.path.join(sim_dir_path, file)):
                     continue
                 elif re.match(sim_file_name, file):
-                    print(f"Removing {file}")
+                    if verbose:
+                        print(f"Removing {file}")
                     os.remove(os.path.join(sim_dir_path, file))
             except:
                 print(f"Failed to remove {file}")
